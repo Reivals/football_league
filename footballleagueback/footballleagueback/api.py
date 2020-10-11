5d5377e814f5733130842cd8fa41d342ba0d4c0b
Mateusz Kawulok <matekaw545@gmail.com>
2020-10-11 15:22:00 +0200

    initial backed match and goal

--- footballleagueback/footballleagueback/api.py
+++ footballleagueback/footballleagueback/api.py
@@ -6,3 +6,5 @@ from league import views
 router = routers.DefaultRouter()
 router.register(r'club', views.ClubViewSet)
 router.register(r'footballer', views.FootballerViewSet)
+router.register(r'match', views.MatchViewSet)
+router.register(r'goal', views.GoalViewSet)
--- footballleagueback/league/views.py
+++ footballleagueback/league/views.py
@@ -1,7 +1,7 @@
 from rest_framework import viewsets
 
 from league import models
-from .serializers import ClubSerializer, FootballerSerializer
+from .serializers import ClubSerializer, FootballerSerializer, MatchSerializer, GoalSerializer
 
 
 class ClubViewSet(viewsets.ModelViewSet):
@@ -11,4 +11,12 @@ class ClubViewSet(viewsets.ModelViewSet):
 class FootballerViewSet(viewsets.ModelViewSet):
     queryset = models.Footballer.objects.all()
     serializer_class = FootballerSerializer
+
+class GoalViewSet(viewsets.ModelViewSet):
+    queryset = models.Goal.objects.all()
+    serializer_class = GoalSerializer
+
+class MatchViewSet(viewsets.ModelViewSet):
+    queryset = models.Match.objects.all()
+    serializer_class = MatchSerializer
 
